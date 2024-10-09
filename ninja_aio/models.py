ad5d5611613f76c75dcaea2dd0c0c585b897bdd0
Caspel26 <giuseppework26@gmail.com>
2024-10-09 02:16:39 +0200

    chore: removed deprecated methods to ModelSerializer, they have been moved to ModelUtil

--- ninja_aio/models.py
+++ ninja_aio/models.py
@@ -224,21 +224,6 @@ class ModelSerializer(models.Model, metaclass=ModelSerializerMeta):
         pass
 
     @classmethod
-    def get_reverse_relations(cls):
-        reverse_rels = []
-        for f in cls.ReadSerializer.fields:
-            field_obj = getattr(cls, f)
-            if isinstance(field_obj, ManyToManyDescriptor):
-                reverse_rels.append(f)
-                continue
-            if isinstance(field_obj, ReverseManyToOneDescriptor):
-                reverse_rels.append(field_obj.field._related_name)
-                continue
-            if isinstance(field_obj, ReverseOneToOneDescriptor):
-                reverse_rels.append(field_obj.related.name)
-        return reverse_rels
-
-    @classmethod
     def get_reverse_relation_schema(
         cls, obj: type["ModelSerializer"], rel_type: type[REL_TYPES], field: str
     ):
@@ -307,59 +292,6 @@ class ModelSerializer(models.Model, metaclass=ModelSerializerMeta):
         return any(field in custom_f for custom_f in customs)
 
     @classmethod
-    async def parse_input_data(cls, request: HttpRequest, data: Schema):
-        payload = data.model_dump()
-        customs = {k: v for k, v in payload.items() if cls.is_custom(k)}
-        for k, v in payload.items():
-            if cls.is_custom(k):
-                continue
-            field_obj = getattr(cls, k).field
-            if isinstance(field_obj, models.BinaryField):
-                try:
-                    payload |= {k: base64.b64decode(v)}
-                except Exception as exc:
-                    raise SerializeError({k: ". ".join(exc.args)}, 400)
-            if isinstance(field_obj, models.ForeignKey):
-                try:
-                    rel: ModelSerializer = await field_obj.related_model.get_object(
-                        request, v
-                    )
-                except ObjectDoesNotExist:
-                    raise SerializeError({k: "not found"}, 404)
-                payload |= {k: rel}
-        new_payload = {k: v for k, v in payload.items() if k not in customs}
-        return new_payload, customs
-
-    @classmethod
-    async def parse_output_data(cls, request: HttpRequest, data: Schema):
-        olds_k: list[dict] = []
-        payload = data.model_dump()
-        for k, v in payload.items():
-            try:
-                field_obj = getattr(cls, k).field
-            except AttributeError:
-                field_obj = getattr(cls, k).related
-            if isinstance(v, dict) and (
-                isinstance(field_obj, models.ForeignKey)
-                or isinstance(field_obj, models.OneToOneField)
-            ):
-                rel: ModelSerializer = await field_obj.related_model.get_object(
-                    request, list(v.values())[0]
-                )
-                if isinstance(field_obj, models.ForeignKey):
-                    for rel_k, rel_v in v.items():
-                        field_rel_obj = getattr(rel, rel_k)
-                        if isinstance(field_rel_obj, models.ForeignKey):
-                            olds_k.append({rel_k: rel_v})
-                    for obj in olds_k:
-                        for old_k, old_v in obj.items():
-                            v.pop(old_k)
-                            v |= {f"{old_k}_id": old_v}
-                    olds_k = []
-                payload |= {k: rel}
-        return payload
-
-    @classmethod
     def get_custom_fields(cls, s_type: type[S_TYPES]):
         try:
             match s_type:
@@ -400,59 +332,3 @@ class ModelSerializer(models.Model, metaclass=ModelSerializerMeta):
             fields=cls.UpdateSerializer.fields,
             custom_fields=cls.get_custom_fields("update"),
         )
-
-    @classmethod
-    async def get_object(cls, request: HttpRequest, pk: int | str):
-        q = {cls._meta.pk.attname: pk}
-        try:
-            obj = (
-                await (await cls.queryset_request(request))
-                .prefetch_related(*cls.get_reverse_relations())
-                .aget(**q)
-            )
-        except ObjectDoesNotExist:
-            raise SerializeError({cls._meta.model_name: "not found"}, 404)
-        return obj
-
-    @classmethod
-    async def create_s(cls, request: HttpRequest, data: Schema):
-        try:
-            payload, customs = await cls.parse_input_data(request, data)
-            pk = (await cls.objects.acreate(**payload)).pk
-            obj = await cls.get_object(request, pk)
-        except SerializeError as e:
-            return e.status_code, e.error
-        payload |= customs
-        await obj.custom_actions(payload)
-        await obj.post_create()
-        return await cls.read_s(request, obj)
-
-    @classmethod
-    async def read_s(cls, request: HttpRequest, obj: type["ModelSerializer"]):
-        schema = cls.generate_read_s().from_orm(obj)
-        return await cls.parse_output_data(request, schema)
-
-    @classmethod
-    async def update_s(cls, request: HttpRequest, data: Schema, pk: int | str):
-        try:
-            obj = await cls.get_object(request, pk)
-        except SerializeError as e:
-            return e.status_code, e.error
-
-        payload, customs = await cls.parse_input_data(request, data)
-        for k, v in payload.items():
-            if v is not None:
-                setattr(obj, k, v)
-        await obj.custom_actions(customs)
-        await obj.asave()
-        updated_object = await cls.get_object(request, pk)
-        return await cls.read_s(request, updated_object)
-
-    @classmethod
-    async def delete_s(cls, request: HttpRequest, pk: int | str):
-        try:
-            obj = await cls.get_object(request, pk)
-        except SerializeError as e:
-            return e.status_code, e.error
-        await obj.adelete()
-        return HttpResponse(status=204)
