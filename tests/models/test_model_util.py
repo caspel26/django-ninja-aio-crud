b68003798726312ebae8cda186e5708d464351fc
Caspel26 <giuseppework26@gmail.com>
2025-01-02 21:29:11 +0100

    WIP: Model util test for every model

--- tests/models/test_model_util.py
+++ tests/models/test_model_util.py
@@ -1,11 +1,38 @@
 from django.test import tag
 
-from tests.test_app import models
+from tests.test_app import models, schema
 from tests.generics.models import Tests
 
 
+class BaseTests:
+    class ModelUtilTestCaseBase(Tests.GenericModelUtilTestCase):
+        @property
+        def create_data(self):
+            return {"name": "test", "description": "test"}
+
+        @property
+        def parsed_input_data(self):
+            return {"payload": self.create_data}
+
+        @property
+        def read_data(self):
+            return {"id": 1, "name": "test", "description": "test"}
+
+    @tag("model_util_model_serializer")
+    class ModelUtilModelSerializerTestCase(ModelUtilTestCaseBase):
+        @property
+        def serializable_fields(self):
+            return self.model.ReadSerializer.fields
+
+    @tag("model_util_model")
+    class ModelUtilModelBaseTestCase(ModelUtilTestCaseBase):
+        @property
+        def serializable_fields(self):
+            return ["id", "name", "description"]
+
+
 @tag("model_util_model_serializer_base")
-class ModelUtilModelSerializerBaseTestCase(Tests.ModelUtilTestCaseBase):
+class ModelUtilModelSerializerBaseTestCase(BaseTests.ModelUtilModelSerializerTestCase):
     model = models.TestModelSerializer
 
     @property
@@ -20,6 +47,18 @@ class ModelUtilModelSerializerBaseTestCase(Tests.ModelUtilTestCaseBase):
     def model_verbose_name_view(self):
         return "testmodelserializers"
 
+
+@tag("model_util_model_base")
+class ModelUtilModelBaseTestCase(BaseTests.ModelUtilModelBaseTestCase):
+    model = models.TestModel
+    schema_in = schema.TestModelSchemaIn
+    schema_out = schema.TestModelSchemaOut
+    schema_patch = schema.TestModelSchemaPatch
+
     @property
-    def create_data(self):
-        return {"name": "test", "description": "test"}
+    def model_verbose_name_path(self):
+        return "test-models"
+
+    @property
+    def model_verbose_name_view(self):
+        return "testmodels"
