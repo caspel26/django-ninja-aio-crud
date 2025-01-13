1f93dc13b01fd8fbd35cce8a5b6fe82ca363a045
Caspel26 <giuseppework26@gmail.com>
2025-01-13 19:18:33 +0100

    chore: handling of pydantic validation error

--- ninja_aio/exceptions.py
+++ ninja_aio/exceptions.py
@@ -3,6 +3,7 @@ from functools import partial
 from joserfc.errors import JoseError
 from ninja import NinjaAPI
 from django.http import HttpRequest, HttpResponse
+from pydantic import ValidationError
 
 
 class BaseException(Exception):
@@ -34,14 +35,29 @@ class AuthError(BaseException):
     pass
 
 
+class PydanticValidationError(BaseException):
+    def __init__(self, details=None):
+        super().__init__("Validation Error", 400, details)
+
+
 def _default_error(
     request: HttpRequest, exc: BaseException, api: type[NinjaAPI]
 ) -> HttpResponse:
     return api.create_response(request, exc.error, status=exc.status_code)
 
 
+def _pydantic_validation_error(
+    request: HttpRequest, exc: ValidationError, api: type[NinjaAPI]
+) -> HttpResponse:
+    error = PydanticValidationError(exc.errors(include_input=False))
+    return api.create_response(request, error.error, status=error.status_code)
+
+
 def set_api_exception_handlers(api: type[NinjaAPI]) -> None:
     api.add_exception_handler(BaseException, partial(_default_error, api=api))
+    api.add_exception_handler(
+        ValidationError, partial(_pydantic_validation_error, api=api)
+    )
 
 
 def parse_jose_error(jose_exc: JoseError) -> dict:
