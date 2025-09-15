7224f0cba203917090bd75806f2ee298a5463c03
Caspel26 <giuseppework26@gmail.com>
2025-09-15 12:07:50 +0200

    feat: add new schemas for M2M operations including add and remove functionality

--- ninja_aio/schemas.py
+++ ninja_aio/schemas.py
@@ -1,5 +1,30 @@
+from uuid import UUID
+from ninja import Schema
 from pydantic import RootModel
 
 
 class GenericMessageSchema(RootModel[dict[str, str]]):
     root: dict[str, str]
+
+
+class M2MDetailSchema(Schema):
+    count: int
+    details: list[str]
+
+
+class M2MSchemaOut(Schema):
+    errors: M2MDetailSchema
+    results: M2MDetailSchema
+
+
+class M2MAddSchemaIn(Schema):
+    add: list[UUID] = []
+
+
+class M2MRemoveSchemaIn(Schema):
+    remove: list[UUID] = []
+
+
+class M2MSchemaIn(Schema):
+    add: list[UUID] = []
+    remove: list[UUID] = []
