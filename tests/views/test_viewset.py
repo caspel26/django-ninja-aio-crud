ab1dfd6bc8a6bbbc6bb72cdfe13f1de9c7e1b899
Caspel26 <giuseppework26@gmail.com>
2026-01-16 16:02:37 +0100

    test: update test_relation_filter_with_none_value to ensure proper object creation

--- tests/views/test_viewset.py
+++ tests/views/test_viewset.py
@@ -774,10 +774,10 @@ class RelationFilterViewSetMixinTestCase(
     async def test_relation_filter_with_none_value(self):
         """Test that None filter values are ignored."""
         await self._drop_all_objects()
-        obj_1 = await self.model.objects.acreate(
+        await self.model.objects.acreate(
             name="obj1", description="desc1", test_model_serializer=self.related_obj_1
         )
-        obj_2 = await self.model.objects.acreate(
+        await self.model.objects.acreate(
             name="obj2", description="desc2", test_model_serializer=self.related_obj_2
         )
         # Filter with None should return all objects
